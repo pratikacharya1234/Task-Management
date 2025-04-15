f630c0e530d4267cbb2604f6c1f32d9aa1ed9541
pratikacharya1234 <acharyapratik214@gmail.com>
2025-04-15 08:25:46 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+# In-memory task storage (replace with file/db later)
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    task = {'name': data['name'], 'date': data['date']}
+    tasks.append(task)
+    return jsonify(tasks)
+
+@app.route('/get_tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
